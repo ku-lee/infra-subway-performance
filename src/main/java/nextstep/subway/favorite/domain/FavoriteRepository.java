d58b22d774e25be1d89408f3cfecde67f1bd9d7b
이고운 <koun@kakao.com>
2021-06-27 17:47:02 +0900

    fix : readOnly 설정추가

--- src/main/java/nextstep/subway/favorite/domain/FavoriteRepository.java
+++ src/main/java/nextstep/subway/favorite/domain/FavoriteRepository.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
 
+    @Transactional(readOnly = true)
     List<Favorite> findByMemberIdOrderByIdDesc(Long memberId, Pageable pageable);
 }
